1f6b9600ced4dedcfcf73eee17ee073faa1721b8
Jihoon Son <jihoonson@apache.org>
2017-08-09 09:25:27 +0900

    testing

--- benchmarks/src/main/java/io/druid/benchmark/query/GroupByBenchmark.java
+++ benchmarks/src/main/java/io/druid/benchmark/query/GroupByBenchmark.java
@@ -118,7 +118,7 @@ public class GroupByBenchmark
   @Param({"4"})
   private int numSegments;
 
-  @Param({"2", "4"})
+  @Param({"4"})
   private int numProcessingThreads;
 
   @Param({"-1"})
@@ -127,13 +127,13 @@ public class GroupByBenchmark
   @Param({"100000"})
   private int rowsPerSegment;
 
-  @Param({"basic.A", "basic.nested"})
+  @Param({"basic.A"})
   private String schemaAndQuery;
 
-  @Param({"v1", "v2"})
+  @Param({"v2"})
   private String defaultStrategy;
 
-  @Param({"all", "day"})
+  @Param({"all"})
   private String queryGranularity;
 
   private static final Logger log = new Logger(GroupByBenchmark.class);
@@ -610,7 +610,7 @@ public class GroupByBenchmark
     }
   }
 
-  @Benchmark
+//  @Benchmark
   @BenchmarkMode(Mode.AverageTime)
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   public void queryMultiQueryableIndexWithSpilling(Blackhole blackhole) throws Exception
@@ -634,7 +634,7 @@ public class GroupByBenchmark
     }
   }
 
-  @Benchmark
+//  @Benchmark
   @BenchmarkMode(Mode.AverageTime)
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   public void queryMultiQueryableIndexWithSerde(Blackhole blackhole) throws Exception
