14078a8fe42960505c0725ce6edc1987dc50831f
Jihoon Son <jihoonson@apache.org>
2018-10-26 21:13:18 -0700

    added timeseries

--- server/src/test/java/org/apache/druid/client/CachingClusteredClientParallelMergeTimeseriesTest.java
+++ server/src/test/java/org/apache/druid/client/CachingClusteredClientParallelMergeTimeseriesTest.java
@@ -22,76 +22,72 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.apache.druid.java.util.common.DateTimes;
 import org.apache.druid.java.util.common.Intervals;
-import org.apache.druid.java.util.common.StringUtils;
 import org.apache.druid.java.util.common.granularity.Granularities;
 import org.apache.druid.query.Result;
 import org.apache.druid.query.TableDataSource;
 import org.apache.druid.query.aggregation.DoubleMaxAggregatorFactory;
 import org.apache.druid.query.aggregation.LongSumAggregatorFactory;
-import org.apache.druid.query.dimension.DefaultDimensionSpec;
 import org.apache.druid.query.spec.MultipleIntervalSegmentSpec;
-import org.apache.druid.query.topn.NumericTopNMetricSpec;
-import org.apache.druid.query.topn.TopNQuery;
-import org.apache.druid.query.topn.TopNResultValue;
+import org.apache.druid.query.timeseries.TimeseriesQuery;
+import org.apache.druid.query.timeseries.TimeseriesResultValue;
 import org.apache.druid.segment.VirtualColumns;
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
-public class CachingClusteredClientParallelMergeTopNTest extends CachingClusteredClientParallelMergeTestBase
+public class CachingClusteredClientParallelMergeTimeseriesTest extends CachingClusteredClientParallelMergeTestBase
 {
   @Override
   void addServers(TestTimelineServerView serverView, int numServers, Random random)
   {
     for (int i = 0; i < numServers; i++) {
-      final List<Result<TopNResultValue>> totalRows = new ArrayList<>();
+      final List<Result<TimeseriesResultValue>> totalRows = new ArrayList<>();
 
       for (int day = 1; day < 6; day++) {
-        final int numRows = random.nextInt(10000) + 10000;
-        final List<Map<String, Object>> rows = new ArrayList<>(numRows);
-        for (int j = 0; j < numRows; j++) {
-          final Map<String, Object> row = createRow(
-              StringUtils.format("dim2_%d", j),
-              random.nextInt(100),
-              random.nextDouble()
-          );
-          rows.add(row);
+        for (int hour = 1; hour < 24; hour++) {
+          for (int minute = 1; minute < 60; minute++) {
+            final Result<TimeseriesResultValue> row = createRow(
+                DateTimes.of("2018-01-%02dT%02d:%02d:00", day, hour, minute),
+                random.nextInt(100),
+                random.nextDouble()
+            );
+            totalRows.add(row);
+          }
         }
-        rows.sort(Comparator.comparing(row -> (String) row.get("dim2")));
-        totalRows.add(new Result<>(DateTimes.of("2018-01-0%d", day), new TopNResultValue(rows)));
       }
 
       serverView.addServer(createServer(i + 1), new TestQueryRunner<>(totalRows));
     }
   }
 
-  private static Map<String, Object> createRow(String dim2, long cnt, double doubleMet)
+  private static Result<TimeseriesResultValue> createRow(DateTime timestamp, long cnt, double doubleMet)
   {
-    return ImmutableMap.of("dim2", dim2, "cnt", cnt, "double_max", doubleMet);
+    return new Result<>(
+        timestamp,
+        new TimeseriesResultValue(ImmutableMap.of("cnt", cnt, "double_max", doubleMet))
+    );
   }
 
   @Test
   public void test()
   {
-    final TopNQuery expectedQuery = new TopNQuery(
+    final TimeseriesQuery expectedQuery = new TimeseriesQuery(
         new TableDataSource(DATA_SOURCE),
-        VirtualColumns.EMPTY,
-        new DefaultDimensionSpec("dim2", "dim2"),
-        new NumericTopNMetricSpec("double_max"),
-        20000, // should be larger than cardinalrity of dim2
         new MultipleIntervalSegmentSpec(Collections.singletonList(Intervals.of("2018-01-01/2018-01-07"))),
+        false,
+        VirtualColumns.EMPTY,
         null,
-        Granularities.DAY,
+        Granularities.MINUTE,
         ImmutableList.of(
             new LongSumAggregatorFactory("cnt", "cnt"),
             new DoubleMaxAggregatorFactory("double_max", "double_met")
         ),
         null,
+        0,
         null
     );
 
