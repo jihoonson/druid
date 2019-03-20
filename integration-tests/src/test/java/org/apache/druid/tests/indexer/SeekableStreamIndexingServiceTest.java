f5532e268b9f6a4d123dee646214b8037c0481ad
Jihoon Son <jihoonson@apache.org>
2019-03-20 15:27:40 -0700

    fix build

--- integration-tests/src/test/java/org/apache/druid/tests/indexer/SeekableStreamIndexingServiceTest.java
+++ integration-tests/src/test/java/org/apache/druid/tests/indexer/SeekableStreamIndexingServiceTest.java
@@ -16,8 +16,15 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.druid.tests.indexer;
 
 public abstract class SeekableStreamIndexingServiceTest extends AbstractIndexerTest
 {
+  public void testQueries()
+  {
+
+  }
+
+  public abstract void generateData();
 }
