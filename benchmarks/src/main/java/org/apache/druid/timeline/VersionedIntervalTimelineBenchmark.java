dcf555d573dc199f2f89d99b183ef7607bd9a8b7
Jihoon Son <jihoonson@apache.org>
2019-07-12 10:48:52 -0700

    add majorly overshadowing segments

--- benchmarks/src/main/java/org/apache/druid/timeline/VersionedIntervalTimelineBenchmark.java
+++ benchmarks/src/main/java/org/apache/druid/timeline/VersionedIntervalTimelineBenchmark.java
@@ -50,12 +50,13 @@ import java.util.concurrent.ThreadLocalRandom;
 @Fork(value = 1, jvmArgsAppend = {"-XX:+UseG1GC", "-agentpath:/Applications/YourKit-Java-Profiler-2019.1.app/Contents/Resources/bin/mac/libyjpagent.jnilib=sampling,onexit=memory"})
 @Warmup(iterations = 10)
 @Measurement(iterations = 10)
-@BenchmarkMode({Mode.AverageTime, Mode.Throughput})
+@BenchmarkMode({Mode.Throughput})
 public class VersionedIntervalTimelineBenchmark
 {
   private static final String DATA_SOURCE = "dataSource";
   private static final Interval TOTAL_INTERVAL = Intervals.of("2018/2019");
   private static final double NEW_ROOT_GEN_SEGMENTS_RATIO_AFTER_COMPACTION = 0.1;
+  private static final double COMPACTED_SEGMENTS_RATIO_TO_INITIAL_SEGMENTS = 0.5;
 
   @Param({"10", "100", "1000"})
   private int numInitialRootGenSegmentsPerInterval;
@@ -63,8 +64,8 @@ public class VersionedIntervalTimelineBenchmark
   @Param({"1", "5"})
   private int numNonRootGenerations;
 
-  @Param({"0.0", "0.5"})
-  private double numCompactedSegmentsRatioToNumInitialSegments;
+  @Param({"false", "true"})
+  private boolean useSegmentLock;
 
   @Param({"MONTH", "DAY"})
   private GranularityType segmentGranularity;
@@ -77,42 +78,52 @@ public class VersionedIntervalTimelineBenchmark
   @Setup
   public void setup()
   {
+    final int numNewRootGenSegmentsAfterCompaction =
+        (int) (numInitialRootGenSegmentsPerInterval * NEW_ROOT_GEN_SEGMENTS_RATIO_AFTER_COMPACTION);
+    final int numCompactedSegments =
+        (int) (numInitialRootGenSegmentsPerInterval * COMPACTED_SEGMENTS_RATIO_TO_INITIAL_SEGMENTS);
+
     intervals = Lists.newArrayList(segmentGranularity.getDefaultGranularity().getIterable(TOTAL_INTERVAL));
     segments = new ArrayList<>(intervals.size() * numInitialRootGenSegmentsPerInterval);
     Map<Interval, Integer> nextRootGenPartitionIds = new HashMap<>(intervals.size());
     Map<Interval, Integer> nextNonRootGenPartitionIds = new HashMap<>(intervals.size());
     Map<Interval, Short> nextMinorVersions = new HashMap<>(intervals.size());
 
-    final String majorVersion = DateTimes.nowUtc().toString();
+    String majorVersion = DateTimes.nowUtc().toString();
 
     for (Interval interval : intervals) {
-      // Generate root generation segments
       int nextRootGenPartitionId = 0;
       int nextNonRootGenPartitionId = PartitionIds.NON_ROOT_GEN_START_PARTITION_ID;
+
+      // Generate root generation segments
       for (int i = 0; i < numInitialRootGenSegmentsPerInterval; i++) {
         segments.add(newSegment(interval, majorVersion, new NumberedShardSpec(nextRootGenPartitionId++, 0)));
       }
 
-      final int numNewRootGenSegmentsAfterCompaction =
-          (int) (numInitialRootGenSegmentsPerInterval * NEW_ROOT_GEN_SEGMENTS_RATIO_AFTER_COMPACTION);
-      final int numCompactedSegments =
-          (int) (numInitialRootGenSegmentsPerInterval * numCompactedSegmentsRatioToNumInitialSegments);
       for (int i = 0; i < numNonRootGenerations; i++) {
+        if (!useSegmentLock) {
+          majorVersion = DateTimes.nowUtc().toString();
+          nextRootGenPartitionId = 0;
+        }
         // Compacted segments
         for (int j = 0; j < numCompactedSegments; j++) {
-          segments.add(
-              newSegment(
-                  interval,
-                  majorVersion,
-                  new NumberedOverwriteShardSpec(
-                      nextNonRootGenPartitionId++,
-                      0,
-                      nextRootGenPartitionId,
-                      (short) (i + 1),
-                      (short) numCompactedSegments
-                  )
-              )
-          );
+          if (useSegmentLock) {
+            segments.add(
+                newSegment(
+                    interval,
+                    majorVersion,
+                    new NumberedOverwriteShardSpec(
+                        nextNonRootGenPartitionId++,
+                        0,
+                        nextRootGenPartitionId,
+                        (short) (i + 1),
+                        (short) numCompactedSegments
+                    )
+                )
+            );
+          } else {
+            segments.add(newSegment(interval, majorVersion, new NumberedShardSpec(nextRootGenPartitionId++, 0)));
+          }
         }
 
         // New segments
@@ -128,11 +139,12 @@ public class VersionedIntervalTimelineBenchmark
     timeline = VersionedIntervalTimeline.forSegments(segments);
 
     newSegments = new ArrayList<>(200);
+
+    // Generate new appending segments
     for (int i = 0; i < 100; i++) {
       final Interval interval = intervals.get(ThreadLocalRandom.current().nextInt(intervals.size()));
       final int rootPartitionId = nextRootGenPartitionIds.get(interval);
-      final int nonRootPartitionId = nextNonRootGenPartitionIds.get(interval);
-      final short minorVersion = nextMinorVersions.get(interval);
+
       newSegments.add(
           newSegment(
               interval,
@@ -140,35 +152,46 @@ public class VersionedIntervalTimelineBenchmark
               new NumberedShardSpec(rootPartitionId, 0)
           )
       );
+      nextRootGenPartitionIds.put(interval, rootPartitionId + 1);
+    }
 
-      if (numCompactedSegmentsRatioToNumInitialSegments > 0.0) {
-        newSegments.add(
-            newSegment(
-                interval,
-                majorVersion,
-                new NumberedOverwriteShardSpec(
-                    nonRootPartitionId,
-                    0,
-                    rootPartitionId,
-                    minorVersion,
-                    (short) 1
-                )
-            )
-        );
+    // Generate overwriting segments
+    if (!useSegmentLock) {
+      majorVersion = DateTimes.nowUtc().toString();
+      nextRootGenPartitionIds.keySet().forEach(interval -> nextRootGenPartitionIds.put(interval, 0));
+    }
 
-        nextRootGenPartitionIds.put(interval, rootPartitionId + 1);
-        nextNonRootGenPartitionIds.put(interval, nonRootPartitionId + 1);
-        nextMinorVersions.put(interval, (short) (minorVersion + 1));
-      } else {
-        newSegments.add(
-            newSegment(
-                interval,
-                majorVersion,
-                new NumberedShardSpec(rootPartitionId + 1, 0)
-            )
-        );
+    final List<Interval> intervalCopies = new ArrayList<>(intervals);
+    for (int i = 0; i < 100 && !intervalCopies.isEmpty(); i++) {
+      final Interval interval = intervalCopies.remove(ThreadLocalRandom.current().nextInt(intervalCopies.size()));
+      int rootPartitionId = nextRootGenPartitionIds.remove(interval);
+      int nonRootPartitionId = nextNonRootGenPartitionIds.remove(interval);
+      final short minorVersion = nextMinorVersions.remove(interval);
 
-        nextRootGenPartitionIds.put(interval, rootPartitionId + 2);
+      for (int j = 0; j < numCompactedSegments; j++) {
+        if (useSegmentLock) {
+          newSegments.add(
+              newSegment(
+                  interval,
+                  majorVersion,
+                  new NumberedOverwriteShardSpec(
+                      nonRootPartitionId++,
+                      0,
+                      rootPartitionId,
+                      minorVersion,
+                      (short) numCompactedSegments
+                  )
+              )
+          );
+        } else {
+          newSegments.add(
+              newSegment(
+                  interval,
+                  majorVersion,
+                  new NumberedShardSpec(rootPartitionId++, 0)
+              )
+          );
+        }
       }
     }
   }
