af9eb8fed0704c053ffc7aabda74f2871cd0aafa
Jihoon Son <jihoonson@apache.org>
2020-03-08 11:18:46 -0700

    close yielder

---
+++ benchmarks/src/test/java/org/apache/druid/HighlyNestedStreamBenchmark.java
@@ -0,0 +1,226 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid;
+
+import com.google.common.collect.FluentIterable;
+import org.apache.druid.java.util.common.guava.Sequence;
+import org.apache.druid.java.util.common.guava.Sequences;
+import org.apache.druid.java.util.common.guava.Yielder;
+import org.apache.druid.java.util.common.guava.Yielders;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+@State(Scope.Benchmark)
+@Fork(value = 1, jvmArgsAppend = {"-XX:+UseG1GC"})
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+@BenchmarkMode({Mode.AverageTime})
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class HighlyNestedStreamBenchmark
+{
+  private static final int NUM_INTEGERS = 1000;
+  private static final int NUM_SETS = 100;
+  private static final int NUM_LIST = 50;
+
+  private List<List<IntegersSet>> integersSetList;
+
+  @Setup
+  public void setup()
+  {
+    integersSetList = new ArrayList<>(NUM_LIST);
+    for (int i = 0; i < NUM_LIST; i++) {
+      List<IntegersSet> innerList = new ArrayList<>(NUM_LIST);
+      for (int j = 0; j < NUM_LIST; j++) {
+        IntegersSet integersSet = new IntegersSet();
+        for (int k = 0; k < NUM_SETS; k++) {
+          Integers integers = new Integers();
+          for (int l = 0; l < NUM_INTEGERS; l++) {
+            integers.add(ThreadLocalRandom.current().nextInt(j + 1));
+          }
+          integersSet.add(integers);
+        }
+        innerList.add(integersSet);
+      }
+      integersSetList.add(innerList);
+    }
+  }
+
+  @Benchmark
+  public void flatMapStream(Blackhole blackhole)
+  {
+    integersSetList.stream()
+                   .flatMap(Collection::stream)
+                   .flatMap(Collection::stream)
+                   .flatMap(Collection::stream)
+                   .forEach(blackhole::consume);
+  }
+
+  @Benchmark
+  public void flatMapIterator(Blackhole blackhole)
+  {
+    FluentIterable.from(integersSetList)
+                  .transformAndConcat(integers -> integers)
+                  .transformAndConcat(integers -> integers)
+                  .transformAndConcat(integers -> integers)
+                  .forEach(blackhole::consume);
+  }
+
+  @Benchmark
+  public void flatMapSequenceYielder(Blackhole blackhole) throws IOException
+  {
+    Sequence<Integer> sequence = Sequences.simple(integersSetList)
+                                          .flatMap(Sequences::simple)
+                                          .flatMap(Sequences::simple)
+                                          .flatMap(Sequences::simple);
+    Yielder<Integer> yielder = Yielders.each(sequence);
+    while (!yielder.isDone()) {
+      final int val = yielder.get();
+      blackhole.consume(val);
+      yielder = yielder.next(null);
+    }
+    yielder.close();
+  }
+
+  @Benchmark
+  public void flatMapSequenceAccumulate(Blackhole blackhole)
+  {
+    Sequence<Integer> sequence = Sequences.simple(integersSetList)
+                                          .flatMap(Sequences::simple)
+                                          .flatMap(Sequences::simple)
+                                          .flatMap(Sequences::simple);
+    sequence.accumulate(null, (accumulated, in) -> {
+      blackhole.consume(in);
+      return null;
+    });
+  }
+
+  @Benchmark
+  public void sumStream(Blackhole blackhole)
+  {
+    int sum = integersSetList.stream()
+                             .flatMap(Collection::stream)
+                             .flatMap(Collection::stream)
+                             .flatMap(Collection::stream)
+                             .mapToInt(i -> i)
+                             .sum();
+    blackhole.consume(sum);
+  }
+
+  @Benchmark
+  public void sumNestedFor(Blackhole blackhole)
+  {
+    int totalSum = 0;
+    for (List<IntegersSet> setList : integersSetList) {
+      for (IntegersSet integersSet : setList) {
+        for (Integers integers : integersSet) {
+          for (Integer i : integers) {
+            totalSum += i;
+          }
+        }
+      }
+    }
+    blackhole.consume(totalSum);
+  }
+
+  @Benchmark
+  public void sumIteratorFlatMap(Blackhole blackhole)
+  {
+    Iterable<Integer> iterable = FluentIterable.from(integersSetList)
+                                               .transformAndConcat(integers -> integers)
+                                               .transformAndConcat(integers -> integers)
+                                               .transformAndConcat(integers -> integers);
+    int sum = 0;
+    for (Integer i : iterable) {
+      sum += i;
+    }
+    blackhole.consume(sum);
+  }
+
+  @Benchmark
+  public void sumSequence(Blackhole blackhole)
+  {
+    Sequence<Integer> sequence = Sequences.simple(integersSetList)
+                                          .flatMap(Sequences::simple)
+                                          .flatMap(Sequences::simple)
+                                          .flatMap(Sequences::simple);
+    blackhole.consume(sequence.accumulate(0, Integer::sum));
+  }
+
+  @Benchmark
+  public void streamToList(Blackhole blackhole)
+  {
+    blackhole.consume(
+        integersSetList.stream()
+                       .flatMap(Collection::stream)
+                       .flatMap(Collection::stream)
+                       .flatMap(Collection::stream)
+                       .collect(Collectors.toList())
+    );
+  }
+
+  @Benchmark
+  public void fluentIteratorToList(Blackhole blackhole)
+  {
+    blackhole.consume(
+        Sequences.simple(integersSetList)
+                 .flatMap(Sequences::simple)
+                 .flatMap(Sequences::simple)
+                 .flatMap(Sequences::simple)
+                 .toList()
+    );
+  }
+
+  @Benchmark
+  public void sequenceToList(Blackhole blackhole)
+  {
+    blackhole.consume(
+        Sequences.simple(integersSetList)
+                 .flatMap(Sequences::simple)
+                 .flatMap(Sequences::simple)
+                 .flatMap(Sequences::simple).toList()
+    );
+  }
+
+  private static class Integers extends ArrayList<Integer>
+  {
+  }
+
+  private static class IntegersSet extends HashSet<Integers>
+  {
+  }
+}
