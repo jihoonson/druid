9430c72cb035887055b1c968f859d092e75970ab
Jihoon Son <jihoonson@apache.org>
2020-06-24 15:40:43 -0700

    lazily populating sequences

--- processing/src/main/java/org/apache/druid/query/RetryQueryRunner.java
+++ processing/src/main/java/org/apache/druid/query/RetryQueryRunner.java
@@ -21,11 +21,11 @@ package org.apache.druid.query;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.druid.java.util.common.guava.BaseSequence;
+import org.apache.druid.java.util.common.guava.BaseSequence.IteratorMaker;
 import org.apache.druid.java.util.common.guava.MergeSequence;
-import org.apache.druid.java.util.common.guava.MergingYielderIterator;
 import org.apache.druid.java.util.common.guava.Sequence;
 import org.apache.druid.java.util.common.guava.Yielder;
-import org.apache.druid.java.util.common.guava.Yielders;
 import org.apache.druid.java.util.common.guava.YieldingAccumulator;
 import org.apache.druid.java.util.common.guava.YieldingSequenceBase;
 import org.apache.druid.java.util.common.logger.Logger;
@@ -33,8 +33,9 @@ import org.apache.druid.query.context.ResponseContext;
 import org.apache.druid.segment.SegmentMissingException;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class RetryQueryRunner<T> implements QueryRunner<T>
 {
@@ -58,41 +59,56 @@ public class RetryQueryRunner<T> implements QueryRunner<T>
   @Override
   public Sequence<T> run(final QueryPlus<T> queryPlus, final ResponseContext context)
   {
-    final Sequence<T> baseSequence = baseRunner.run(queryPlus, context);
+//    final List<Sequence<T>> listOfSequences = new ArrayList<>();
+//    listOfSequences.add(baseRunner.run(queryPlus, context));
 
     return new YieldingSequenceBase<T>()
     {
       @Override
       public <OutType> Yielder<OutType> toYielder(OutType initValue, YieldingAccumulator<OutType, T> accumulator)
       {
-        final List<Yielder<T>> yielders = new ArrayList<>();
-        yielders.add(Yielders.each(baseSequence));
+//        List<SegmentDescriptor> missingSegments = getMissingSegments(context);
+//
+//        if (!missingSegments.isEmpty()) {
+//          for (int i = 0; i < config.getNumTries() && !missingSegments.isEmpty(); i++) {
+//            LOG.info("[%,d] missing segments found. Retry attempt [%,d]", missingSegments.size(), i);
+//
+//            context.put(ResponseContext.Key.MISSING_SEGMENTS, new ArrayList<>());
+//            final QueryPlus<T> retryQueryPlus = queryPlus.withQuery(
+//                Queries.withSpecificSegments(queryPlus.getQuery(), missingSegments)
+//            );
+//            Sequence<T> retrySequence = baseRunner.run(retryQueryPlus, context);
+//            listOfSequences.add(retrySequence);
+//            missingSegments = getMissingSegments(context);
+//          }
+//
+//          final List<SegmentDescriptor> finalMissingSegs = getMissingSegments(context);
+//          if (!config.isReturnPartialResults() && !finalMissingSegs.isEmpty()) {
+//            throw new SegmentMissingException("No results found for segments[%s]", finalMissingSegs);
+//          }
+//
+//          return new MergeSequence<>(queryPlus.getQuery().getResultOrdering(), Sequences.simple(listOfSequences))
+//              .toYielder(initValue, accumulator);
+//        } else {
+//          return Iterables.getOnlyElement(listOfSequences).toYielder(initValue, accumulator);
+//        }
 
-        List<SegmentDescriptor> missingSegments = getMissingSegments(context);
+        final Sequence<Sequence<T>> sequence = new BaseSequence<>(
+            new IteratorMaker<Sequence<T>, LazySequenceIterator>()
+            {
+              @Override
+              public LazySequenceIterator make()
+              {
+                return new LazySequenceIterator(queryPlus, context, baseRunner.run(queryPlus, context));
+              }
 
-        if (!missingSegments.isEmpty()) {
-          for (int i = 0; i < config.getNumTries() && !missingSegments.isEmpty(); i++) {
-            LOG.info("[%,d] missing segments found. Retry attempt [%,d]", missingSegments.size(), i);
-
-            context.put(ResponseContext.Key.MISSING_SEGMENTS, new ArrayList<>());
-            final QueryPlus<T> retryQueryPlus = queryPlus.withQuery(
-                Queries.withSpecificSegments(queryPlus.getQuery(), missingSegments)
-            );
-            Sequence<T> retrySequence = baseRunner.run(retryQueryPlus, context);
-            yielders.add(Yielders.each(retrySequence));
-            missingSegments = getMissingSegments(context);
-          }
-
-          final List<SegmentDescriptor> finalMissingSegs = getMissingSegments(context);
-          if (!config.isReturnPartialResults() && !finalMissingSegs.isEmpty()) {
-            throw new SegmentMissingException("No results found for segments[%s]", finalMissingSegs);
-          }
-        }
-        return MergeSequence.makeYielder(
-            new MergingYielderIterator<>(queryPlus.getQuery().getResultOrdering(), yielders),
-            initValue,
-            accumulator
+              @Override
+              public void cleanup(LazySequenceIterator iterFromMake)
+              {
+              }
+            }
         );
+        return new MergeSequence<>(queryPlus.getQuery().getResultOrdering(), sequence).toYielder(initValue, accumulator);
       }
     };
   }
@@ -101,7 +117,7 @@ public class RetryQueryRunner<T> implements QueryRunner<T>
   {
     final Object maybeMissingSegments = context.get(ResponseContext.Key.MISSING_SEGMENTS);
     if (maybeMissingSegments == null) {
-      return Collections.emptyList();
+      return new ArrayList<>();
     }
 
     return jsonMapper.convertValue(
@@ -110,5 +126,55 @@ public class RetryQueryRunner<T> implements QueryRunner<T>
         {
         }
     );
+  }
+
+  private class LazySequenceIterator implements Iterator<Sequence<T>>
+  {
+    private final QueryPlus<T> queryPlus;
+    private final ResponseContext context;
+    private Sequence<T> sequence;
+    private int retryCount = 0;
+
+    private LazySequenceIterator(QueryPlus<T> queryPlus, ResponseContext context, Sequence<T> baseSequence)
+    {
+      this.queryPlus = queryPlus;
+      this.context = context;
+      this.sequence = baseSequence;
+    }
+
+    @Override
+    public boolean hasNext()
+    {
+      if (sequence != null) {
+        return true;
+      } else {
+        final List<SegmentDescriptor> missingSegments = getMissingSegments(context);
+        if (missingSegments.isEmpty()) {
+          return false;
+        } else if (retryCount >= config.getNumTries() && !config.isReturnPartialResults()) {
+          throw new SegmentMissingException("No results found for segments[%s]", missingSegments);
+        } else {
+          LOG.info("[%,d] missing segments found. Retry attempt [%,d]", missingSegments.size(), retryCount++);
+
+          context.put(ResponseContext.Key.MISSING_SEGMENTS, new ArrayList<>());
+          final QueryPlus<T> retryQueryPlus = queryPlus.withQuery(
+              Queries.withSpecificSegments(queryPlus.getQuery(), missingSegments)
+          );
+          sequence = baseRunner.run(retryQueryPlus, context);
+          return true;
+        }
+      }
+    }
+
+    @Override
+    public Sequence<T> next()
+    {
+      if (!hasNext()) {
+        throw new NoSuchElementException();
+      }
+      final Sequence<T> next = sequence;
+      sequence = null;
+      return next;
+    }
   }
 }
